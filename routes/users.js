b912eb68e0fbc173d26684b2291554722abf3ff9
CyanBlueCode <CHASE@Chases-MBP.attlocal.net>
2019-11-24 09:55:15 -0800

    (progress) test route setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.json([
+    {
+      id: 1,
+      name: 'black dynamite'
+    }
+  ]);
 });
 
 module.exports = router;
