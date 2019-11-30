950ab042d2946b4123ea8104872634616ec04575
CyanBlueCode <CHASE@Chases-MacBook-Pro.local>
2019-11-29 18:21:57 -0800

    (update) remove unnecessary views files & general clean up

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,50 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const { google } = require('googleapis');
+const keys = require('../gkeys.json');
+
+/* GET data from Google Sheets API */
+router.get('/umma-server/getData', (req, res) => {
+  const client = new google.auth.JWT(
+    keys.client_email,
+    null,
+    keys.private_key,
+    ['https://www.googleapis.com/auth/spreadsheets.readonly']
+  );
+
+  async function gsGet(clientAuth) {
+    const gsApi = google.sheets({
+      version: 'v4',
+      auth: clientAuth
+    });
+
+    const options = {
+      spreadsheetId: '1JWYHiOightRfEg3qFrDniItyYII5fgXdi99m11mgSH0',
+      ranges: ['Projects!A4:I', 'Events!A4:G']
+    };
+
+    const data = await gsApi.spreadsheets.values.batchGet(options);
+
+    const projects = data.data.valueRanges[0].values;
+    const events = data.data.valueRanges[1].values;
+
+    const sheetsData = {
+      projects,
+      events
+    };
+
+    return sheetsData;
+  }
+
+  client.authorize((err, tokens) => {
+    if (err) {
+      console.error(err, tokens);
+      throw err;
+    } else {
+      gsGet(client).then(data => res.json(data));
+    }
+  });
 });
 
 module.exports = router;
